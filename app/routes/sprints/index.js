5c3fdda92ae008970980e5541d83b885c7997409
Nelson Páez <nelpa90@gmail.com>
2016-07-02 05:48:31 -0400

    Lógica de Sprints en proyecto

--- app/routes/sprints/index.js
+++ app/routes/sprints/index.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    proyecto: {
+      refreshModel: true
+    }
+  },
   actions: {
     remove: function(model) {
       if(confirm('Are you sure?')) {
@@ -8,7 +13,11 @@ export default Ember.Route.extend({
       }
     }
   },
-  model: function() {
-    return this.store.findAll('sprint');
+  model: function(params) {
+    if(params.proyecto){
+      return this.store.query('sprint', params);
+    }else {
+      return this.store.findAll('sprint');
+    }
   }
 });
---
+++ tests/unit/routes/sprints/show-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:sprints/show', 'Unit | Route | sprints/show', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
