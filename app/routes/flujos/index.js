88e33cf47bd17adfef8d8479736052a15c650680
developer <dbaezrenault@gmail.com>
2016-07-02 04:54:01 -0400

    Eliminados flujo, sprint y userstory del menu principal

--- app/routes/flujos/index.js
+++ app/routes/flujos/index.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    proyecto: {
+      refreshModel: true
+    }
+  },
   actions: {
     remove: function(model) {
       if(confirm('Are you sure?')) {
@@ -8,7 +13,11 @@ export default Ember.Route.extend({
       }
     }
   },
-  model: function() {
-    return this.store.findAll('flujo');
+  model: function(params) {
+    if(params.proyecto){
+      return this.store.query('flujo', params);
+    }else {
+      return this.store.findAll('flujo');
+    }
   }
 });
