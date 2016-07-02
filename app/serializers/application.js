f4775dba98e449546e29659eac6e91b5db1f127d
Nelson Páez <nelpa90@gmail.com>
2016-07-02 03:11:39 -0400

    Corrección del error para belongsTo

---
+++ app/serializers/application.js
@@ -0,0 +1,35 @@
+import JSONAPISerializer from 'ember-data/serializers/json-api';
+
+export default JSONAPISerializer.extend({
+
+  /**
+   @method serializeBelongsTo
+   @param {DS.Snapshot} snapshot
+   @param {Object} json
+   @param {Object} relationship
+  */
+  serializeBelongsTo(snapshot, json, relationship) {
+    var key = relationship.key;
+
+    if (this._canSerialize(key)) {
+      var belongsTo = snapshot.belongsTo(key);
+      if (belongsTo !== undefined) {
+
+        json.attributes = json.attributes || {};
+
+        var payloadKey = this._getMappedKey(key, snapshot.type);
+        if (payloadKey === key) {
+          payloadKey = this.keyForRelationship(key, 'belongsTo', 'serialize');
+        }
+
+        let data = null;
+        if (belongsTo) {
+          data = belongsTo.id
+        }
+
+        json.attributes[payloadKey] = data;
+      }
+    }
+  },
+
+});
