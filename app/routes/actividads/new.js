aa1a1a44c6b2627a4124421e23b2290b84b19bfa
Nelson Páez <nelpa90@gmail.com>
2016-07-02 09:11:22 -0400

    Agregar actividades desde kanban

--- app/routes/actividads/new.js
+++ app/routes/actividads/new.js
@@ -2,7 +2,20 @@ import Ember from 'ember';
 import SaveModelMixin from '../../mixins/actividads/save-model-mixin';
 
 export default Ember.Route.extend(SaveModelMixin, {
-  model: function() {
-    return this.store.createRecord('actividad');
+  queryParams: {
+    flujo: {
+      refreshModel: true
+    }
+  },
+  model: function(params) {
+    var nuevo = this.store.createRecord('actividad');
+
+    if(params.flujo){
+      this.store.find('flujo', params.flujo).then(function(flujo) {
+        nuevo.set('flujo', flujo);
+      });
+    }
+
+    return nuevo;
   }
 });
